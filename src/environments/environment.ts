80a94e8d08eaf7d37d14f16e4ca3ab1aae3d109b
Dimitri <dimpirogovp@hotmail.com>
2024-07-05 14:02:43 +0200

    changed environment2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   RAPIDAPI_KEY: process.env['RAPIDAPI_KEY'],
-  rapidHostUrl: 'imdb8.p.rapidapi.com',
+  rapidHostUrl: process.env['imdb8.p.rapidapi.com'],
   serverUrl: 'http://localhost:8000',
   testBaseUrl: 'http://localhost:3000',
 };
