3c831c53f4a6cc7e5e8d425a266275c5776ac056
Dimitri <dimpirogovp@hotmail.com>
2024-07-05 21:26:24 +0200

    corrected env variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   RAPIDAPI_KEY: process.env['RAPIDAPI_KEY'],
-  rapidHostUrl: process.env['RAPIDAPI_HOST'],
+  RAPIDAPI_HOST: process.env['RAPIDAPI_HOST'],
   serverUrl: 'http://localhost:8000',
   testBaseUrl: 'http://localhost:3000',
 };
