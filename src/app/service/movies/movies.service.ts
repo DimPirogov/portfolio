8c663fa468c7776c508b3e48d24fd0bc3847bb45
Dimitri <dimpirogovp@hotmail.com>
2024-07-05 14:02:29 +0200

    changed environment

--- src/app/service/movies/movies.service.ts
+++ src/app/service/movies/movies.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
 
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
